98bc295ae7522f2b1d7315c522e8f54fb3d97cb8
MerpatiBiru78 <merpatibiru.78@gmail.com>
2023-03-10 16:37:58 +0700

    footer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -67,19 +67,18 @@
         </button>
     </div>
     <div class="container bg-light ">
-        <div class="row py-3 px-5 bg-success bg-gradient align-items-md-stretch ">
-            <div class="col-md-4 "> 
+        <div class="row py-3 px-5 bg-success  align-items-md-stretch ">
+
+            <div class="col-md-4 ">
                 <div class="h-100 p-5 bg-light text-dark shadow">
                     <h2>Tentang Kami</h2>
                     <br>
-                    <p class="text-start text-wrap" >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius placeat quas laborum id, at porro
-                        quasi odio vitae aut ad architecto? Consectetur, atque in molestias sint porro perferendis ab at
-                        quam sequi! Ex laboriosam mollitia, ipsa veritatis quasi eos debitis quam fuga et explicabo eius
-                        deleniti doloremque sequi, eligendi consectetur temporibus repudiandae impedit, nihil consequatur
-                        facilis placeat. Iusto earum repellendus itaque error odio. Illo est pariatur atque ex possimus.
-                        Aspernatur modi sed culpa deleniti illum totam, obcaecati sapiente! Recusandae a maxime, quidem
-                        dignissimos deserunt at voluptatem veniam voluptates iste amet animi harum eius consectetur earum!
+                    <p class="text-start text-wrap">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia voluptatibus amet, fuga similique
+                        est quidem nisi necessitatibus, laborum animi ad maxime deserunt natus dolore inventore commodi
+                        ipsa, rerum aperiam! Velit unde veritatis quis distinctio libero eos blanditiis numquam laboriosam
+                        ut! Ducimus dolores facere repellendus est. Officiis, dignissimos magnam vitae molestias, ex error
+                        eligendi labore eaque aliquid officia veritatis.
                     </p>
                 </div>
             </div>
@@ -89,7 +88,7 @@
                 </div>
             </div>
         </div>
-        <div class="row pt-3 align-items-md-stretch justify-content-between">
+        <div class="row px-3 py-3 align-items-md-stretch justify-content-between">
             <div class="col-md-6">
                 <div class="h-100 p-5 bg-lg text-dark rounded-3">
                     <h2>VISI</h2>
@@ -99,16 +98,85 @@
                         kemerdekaan yaitu terwujudnya kesejahteraan dan tegaknya keadilan bagi sulurh Bangsa Indonesia tanpa
                         terkecuali
                     </p>
-                    {{-- <button class="btn btn-outline-light" type="button">Example button</button> --}}
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="h-100 p-5 bg-light border rounded-3">
+                <div class="h-100 p-5 bg-emerald text-light border rounded-3">
                     <h2>MISI</h2>
-                    <p></p>
-                    {{-- <button class="btn btn-outline-secondary" type="button">Example button</button> --}}
+                    <p>
+                        Melaksanakan <strong>Sistem</strong> yang Berlaku di <strong>Lembaga Pendidikan Unggulan Amanatul
+                            Ummah</strong> Secara ketat dan Bertangung Jawab
+                    </p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container-fluid bg-light bg-gradient">
+        <footer class="py-5 px-5">
+            <div class="row">
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Section</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Section</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-6 col-md-2 mb-3">
+                    <h5>Section</h5>
+                    <ul class="nav flex-column">
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-5 offset-md-1 mb-3">
+                    <form>
+                        <h5>Subscribe to our newsletter</h5>
+                        <p>Monthly digest of what's new and exciting from us.</p>
+                        <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                            <label for="newsletter1" class="visually-hidden">Email address</label>
+                            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+                            <button class="btn btn-primary" type="button">Subscribe</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+                <p>© 2022 Company, Inc. All rights reserved.</p>
+                <ul class="list-unstyled d-flex">
+                    <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24"
+                                height="24">
+                                <use xlink:href="#twitter"></use>
+                            </svg></a></li>
+                    <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24"
+                                height="24">
+                                <use xlink:href="#instagram"></use>
+                            </svg></a></li>
+                    <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24"
+                                height="24">
+                                <use xlink:href="#facebook"></use>
+                            </svg></a></li>
+                </ul>
+            </div>
+        </footer>
     </div>
 @endsection
