70aa42f0db6162e9f284bb6793095f05d611139d
MBMIUN\Mi'un <merpatibiru.78@gmail.com>
2023-03-10 07:54:11 +0700

    start awal

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ 'css/style.css' }}">
+    <link rel="stylesheet" href="{{ 'css/bootstrap.min.css' }}">
+    <link rel="stylesheet" href="{{ 'FA6pro/css/all.min.css' }}">
+    <title>@yield('title',$title)</title>
+</head>
+<body class="body">
+    @yield('content')
+    <script src="{{ 'FA6pro/js/all.min.js' }}"></script>
+    <script src="{{ 'js/bootstrap.bundle.min.js' }}"></script>
+</body>
+</html>
