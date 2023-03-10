47554ffef93e155eb056d6713c3b74c9f7d4d4ca
MerpatiBiru78 <merpatibiru.78@gmail.com>
2023-03-10 15:01:10 +0700

    banner

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -48,13 +48,10 @@
     <div id="carouselExampleFade" class="carousel slide carousel-fade">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="{{ 'image/LOGO MTSU.png' }}" class="d-block w-100" alt="...">
+            <img src="{{ 'image/banner web 1.jpg' }}" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
+            <img src="{{ 'image/banner web 2.jpg' }}" class="d-block w-100" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
