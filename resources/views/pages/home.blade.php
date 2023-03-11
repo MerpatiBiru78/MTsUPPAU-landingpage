c0594c654b786e9aabb93e8ba28be67a55ad3b2f
MerpatiBiru78 <merpatibiru.78@gmail.com>
2023-03-11 14:08:33 +0700

    art

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -141,171 +141,84 @@
         <div class="row g-5">
             {{-- main --}}
             <div class="col-md-8">
-                <h3 class="pb-4 mb-4 fst-italic border-bottom">
-                    From the Firehose
+                <h3 class="pb-4 mb-4 border-bottom">
+                    MTs Unggulan Osis Article
                 </h3>
-
+                <form class="d-flex justify-content-end py-4 " role="search">
+                    <input class="form-control me-2  w-25" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit"><i class="fad fa-search" aria-hidden="true"></i>    </button>
+                </form>
                 <article class="blog-post">
-                    <h2 class="blog-post-title mb-1">Sample blog post</h2>
-                    <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
-
-                    <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap.
-                        Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
-                    <hr>
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <h2>Blockquotes</h2>
-                    <p>This is an example blockquote in action:</p>
-                    <blockquote class="blockquote">
-                        <p>Quoted text goes here.</p>
-                    </blockquote>
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <h3>Example lists</h3>
-                    <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the
-                        other
-                        highly repetitive body text used throughout. This is an example unordered list:</p>
-                    <ul>
-                        <li>First list item</li>
-                        <li>Second list item with a longer description</li>
-                        <li>Third list item to close it out</li>
-                    </ul>
-                    <p>And this is an ordered list:</p>
-                    <ol>
-                        <li>First list item</li>
-                        <li>Second list item with a longer description</li>
-                        <li>Third list item to close it out</li>
-                    </ol>
-                    <p>And this is a definition list:</p>
-                    <dl>
-                        <dt>HyperText Markup Language (HTML)</dt>
-                        <dd>The language used to describe and define the content of a Web page</dd>
-                        <dt>Cascading Style Sheets (CSS)</dt>
-                        <dd>Used to describe the appearance of Web content</dd>
-                        <dt>JavaScript (JS)</dt>
-                        <dd>The programming language used to build advanced Web sites and applications</dd>
-                    </dl>
-                    <h2>Inline HTML elements</h2>
-                    <p>HTML defines a long list of available inline tags, a complete list of which can be found on the
-                        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element">Mozilla Developer
-                            Network</a>.
-                    </p>
-                    <ul>
-                        <li><strong>To bold text</strong>, use <code
-                                class="language-plaintext highlighter-rouge">&lt;strong&gt;</code>.</li>
-                        <li><em>To italicize text</em>, use <code
-                                class="language-plaintext highlighter-rouge">&lt;em&gt;</code>.</li>
-                        <li>Abbreviations, like <abbr title="HyperText Markup Language">HTML</abbr> should use <code
-                                class="language-plaintext highlighter-rouge">&lt;abbr&gt;</code>, with an optional
-                            <code class="language-plaintext highlighter-rouge">title</code> attribute for the full
-                            phrase.
-                        </li>
-                        <li>Citations, like <cite>— Mark Otto</cite>, should use <code
-                                class="language-plaintext highlighter-rouge">&lt;cite&gt;</code>.</li>
-                        <li><del>Deleted</del> text should use <code
-                                class="language-plaintext highlighter-rouge">&lt;del&gt;</code> and <ins>inserted</ins>
-                            text should use <code class="language-plaintext highlighter-rouge">&lt;ins&gt;</code>.</li>
-                        <li>Superscript <sup>text</sup> uses <code
-                                class="language-plaintext highlighter-rouge">&lt;sup&gt;</code> and subscript
-                            <sub>text</sub> uses <code class="language-plaintext highlighter-rouge">&lt;sub&gt;</code>.
-                        </li>
-                    </ul>
-                    <p>Most of these elements are styled by browsers with few modifications on our part.</p>
-                    <h2>Heading</h2>
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <h3>Sub-heading</h3>
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <pre><code>Example code block</code></pre>
-                    <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the
-                        other
-                        highly repetitive body text used throughout.</p>
+                    <div class="card mb-3">
+                        <img src="{{ 'image/banner web 1.jpg' }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Unde,
+                                consectetur at explicabo magni nemo suscipit quibusdam optio maxime delectus reiciendis,
+                                vero ea minima? Esse porro quidem molestiae! Molestiae delectus esse quo tempora
+                                praesentium, facilis harum impedit perferendis est eos, sunt dignissimos! Laborum neque
+                                velit iusto dolorem ab corporis omnis nesciunt. Aspernatur obcaecati, aperiam cum, nobis
+                                dolorum facere ad velit hic eum, expedita modi architecto. Dolor facilis aliquid iste?
+                                Ratione tempora accusamus expedita quod soluta quidem eos nesciunt consectetur aspernatur
+                                tempore, fuga illum repudiandae magni quibusdam sapiente nihil velit distinctio, sit iure
+                                dicta provident! Numquam cupiditate libero eius, itaque delectus quod.</p>
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text text-start"><small class="text-muted">Last updated 3 mins ago</small>
+                                </p>
+                                <a href="#" class="btn btn-outline-secondary">Baca Lebih Lanjut</a>
+                            </div>
+                        </div>
+                    </div>
                 </article>
 
                 <article class="blog-post">
-                    <h2 class="blog-post-title mb-1">Another blog post</h2>
-                    <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p>
-
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <blockquote>
-                        <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.
-                        </p>
-                    </blockquote>
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <h3>Example table</h3>
-                    <p>And don't forget about tables in these posts:</p>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Upvotes</th>
-                                <th>Downvotes</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Alice</td>
-                                <td>10</td>
-                                <td>11</td>
-                            </tr>
-                            <tr>
-                                <td>Bob</td>
-                                <td>4</td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td>Charlie</td>
-                                <td>7</td>
-                                <td>9</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td>Totals</td>
-                                <td>21</td>
-                                <td>23</td>
-                            </tr>
-                        </tfoot>
-                    </table>
-
-                    <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the
-                        other
-                        highly repetitive body text used throughout.</p>
+                    <div class="card mb-3">
+                        <img src="{{ 'image/banner web 1.jpg' }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Unde,
+                                consectetur at explicabo magni nemo suscipit quibusdam optio maxime delectus reiciendis,
+                                vero ea minima? Esse porro quidem molestiae! Molestiae delectus esse quo tempora
+                                praesentium, facilis harum impedit perferendis est eos, sunt dignissimos! Laborum neque
+                                velit iusto dolorem ab corporis omnis nesciunt. Aspernatur obcaecati, aperiam cum, nobis
+                                dolorum facere ad velit hic eum, expedita modi architecto. Dolor facilis aliquid iste?
+                                Ratione tempora accusamus expedita quod soluta quidem eos nesciunt consectetur aspernatur
+                                tempore, fuga illum repudiandae magni quibusdam sapiente nihil velit distinctio, sit iure
+                                dicta provident! Numquam cupiditate libero eius, itaque delectus quod.</p>
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text text-start"><small class="text-muted">Last updated 3 mins ago</small>
+                                </p>
+                                <a href="#" class="btn btn-outline-secondary">Baca Lebih Lanjut</a>
+                            </div>
+                        </div>
+                    </div>
                 </article>
 
                 <article class="blog-post">
-                    <h2 class="blog-post-title mb-1">New feature</h2>
-                    <p class="blog-post-meta">December 14, 2020 by <a href="#">Chris</a></p>
-
-                    <p>This is some additional paragraph placeholder content. It has been written to fill the available
-                        space and show how a longer snippet of text affects the surrounding content. We'll repeat it
-                        often
-                        to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-                    <ul>
-                        <li>First list item</li>
-                        <li>Second list item with a longer description</li>
-                        <li>Third list item to close it out</li>
-                    </ul>
-                    <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the
-                        other
-                        highly repetitive body text used throughout.</p>
+                    <div class="card mb-3">
+                        <img src="{{ 'image/banner web 1.jpg' }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Unde,
+                                consectetur at explicabo magni nemo suscipit quibusdam optio maxime delectus reiciendis,
+                                vero ea minima? Esse porro quidem molestiae! Molestiae delectus esse quo tempora
+                                praesentium, facilis harum impedit perferendis est eos, sunt dignissimos! Laborum neque
+                                velit iusto dolorem ab corporis omnis nesciunt. Aspernatur obcaecati, aperiam cum, nobis
+                                dolorum facere ad velit hic eum, expedita modi architecto. Dolor facilis aliquid iste?
+                                Ratione tempora accusamus expedita quod soluta quidem eos nesciunt consectetur aspernatur
+                                tempore, fuga illum repudiandae magni quibusdam sapiente nihil velit distinctio, sit iure
+                                dicta provident! Numquam cupiditate libero eius, itaque delectus quod.</p>
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text text-start"><small class="text-muted">Last updated 3 mins ago</small>
+                                </p>
+                                <a href="#" class="btn btn-outline-secondary">Baca Lebih Lanjut</a>
+                            </div>
+                        </div>
+                    </div>
                 </article>
-
                 <nav class="blog-pagination" aria-label="Pagination">
                     <a class="btn btn-outline-primary rounded-pill" href="#">Older</a>
                     <a class="btn btn-outline-secondary rounded-pill disabled">Newer</a>
@@ -317,6 +230,5 @@
         </div>
     </main>
     {{-- footer --}}
-@include('partials.footerFront')
+    @include('partials.footerFront')
 @endsection
-
