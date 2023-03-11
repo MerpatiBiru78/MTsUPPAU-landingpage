b707a26ea853fdda8cf30a328c7d7f771ba1b196
MerpatiBiru78 <merpatibiru.78@gmail.com>
2023-03-11 11:15:03 +0700

    web pages oke2

---
+++ resources/views/partials/headerFront.blade.php
@@ -0,0 +1,48 @@
+    <header class="mx-4 my-2 d-inline-flex ">
+        <a href="#" class="navbar-brand d-flex gap-1">
+            <img src="{{ 'image/LOGO MTSU.png' }}" alt="MTs Unggulan PP Amanatul Ummah" height="40">
+            <h5 class="text-light">MTs Unggulan<br>PP Amanatul Ummah</h5>
+        </a>
+    </header>
+    <nav class="mx-4 py-2 navbar navbar-expand-lg bg-body-tertiary rounded shadow color-dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav nav-links">
+                    <li class="nav-item">
+                        <a class="nav-link {{ Request::path() === '/' ? 'active' : '' }}" aria-current="page"
+                            href="#"><i class="fad fa-home"></i> Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fad fa-users"></i> Tentang Kami</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fad fa-images"></i> Galeri</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link "><i class="fad fa-medal"></i> Prestasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link "><i class="fad fa-chalkboard-user"></i> Pengajar</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <i class="fad fa-landmark"></i> Program Unggulan
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><i class="fad fa-medal"></i> Kelas
+                                    <strong>UNGGULAN</strong></a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fad fa-gauge-max"></i> Kelas SKS 2
+                                    Tahun</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fad fa-gauge-high"></i> Kelas SKS 3
+                                    Tahun</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ resources/views/partials/rightSideFront.blade.php
@@ -0,0 +1,48 @@
+<div class="col-md-4">
+    <div class="position-sticky" style="top: 2rem;">
+        <div class="p-4 mb-3 bg-success bg-gradient text-light rounded">
+            <h4 class="text-uppercase">visi</h4>
+            <p class="mb-0 text-justify">Terwujudnya manusia yang <strong> Unggul, Utuh, dan Berakhlakul
+                    Karimah </strong> untuk kemuliaan dan kejayaan Islam dan kaum Muslimin, kemulian dan
+                kejayaan seluruh bangsa Indonesia dan untuk keberhasilan cita-cita kemerdekaan yaitu
+                terwujudnya
+                kesejahteraan dan tegaknya keadilan bagi sulurh Bangsa Indonesia tanpa terkecuali
+            </p>
+        </div>
+        <div class="p-4 mb-3 bg-warning bg-gradient text-light rounded">
+            <h4 class="text-uppercase">misi</h4>
+            <p class="mb-0 text-justify"> Melaksanakan <strong>Sistem</strong> yang Berlaku di
+                <strong>Lembaga
+                    Pendidikan Unggulan Amanatul
+                    Ummah</strong> Secara ketat dan Bertangung Jawab
+            </p>
+        </div>
+
+        <div class="p-4">
+            <h4 class="fst-italic">Archives</h4>
+            <ol class="list-unstyled mb-0">
+                <li><a href="#">March 2021</a></li>
+                <li><a href="#">February 2021</a></li>
+                <li><a href="#">January 2021</a></li>
+                <li><a href="#">December 2020</a></li>
+                <li><a href="#">November 2020</a></li>
+                <li><a href="#">October 2020</a></li>
+                <li><a href="#">September 2020</a></li>
+                <li><a href="#">August 2020</a></li>
+                <li><a href="#">July 2020</a></li>
+                <li><a href="#">June 2020</a></li>
+                <li><a href="#">May 2020</a></li>
+                <li><a href="#">April 2020</a></li>
+            </ol>
+        </div>
+
+        <div class="p-4">
+            <h4 class="fst-italic">Elsewhere</h4>
+            <ol class="list-unstyled">
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Facebook</a></li>
+            </ol>
+        </div>
+    </div>
+</div>
