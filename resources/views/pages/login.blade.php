b867eeca9caa36ea6fcc5ba6df141d813c60f8e7
MerpatiBiru78 <merpatibiru.78@gmail.com>
2023-03-14 11:07:03 +0700

    bg login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,14 +1,14 @@
 @extends('back')
 @section('login')
-    <main class="d-flex justify-content-center p-4 bg-wave">
+    <main class="d-flex justify-content-center p-4">
         <section class="wrapper bg-lg">
             <div class="form signup ">
                 <header>Login</header>
                 <form action="#">
-                    <input type="text" placeholder="Email address" required />
+                    <input type="email" placeholder="Email address" id="exampleFormControlInput1" required />
                     <input type="password" placeholder="Password" required />
                     <a href="#">Forgot password?</a>
-                    <input type="submit" class="btn btn-success" value="Login" />
+                    <input type="submit" class="btn bg-emerald btn-success" value="Login" />
                 </form>
 
             </div>
@@ -16,14 +16,14 @@
             <div class="form login bg-emerald align-content-center">
                 <header>Signup</header>
                 <form action="#">
-                    <input type="text" placeholder="Full name" required />
+                    <input type="text" placeholder="Full name" id="exampleFormControlInput1" required />
                     <input type="text" placeholder="Email address" required />
                     <input type="password" placeholder="Password" required />
                     <div class="checkbox">
                         <input type="checkbox" id="signupCheck" />
                         <label for="signupCheck">I accept all terms & conditions</label>
                     </div>
-                    <input type="submit" class="mb-5 btn btn-primary" value="Signup" />
+                    <input type="submit" class="mb-5 btn bg-lg btn-success" value="Signup" />
                 </form>
             </div>
 
